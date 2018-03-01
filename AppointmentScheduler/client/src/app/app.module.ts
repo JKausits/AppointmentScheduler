d3dd6a118ea06e4e454182c951f60eae03851957
Joe <JoeKausits@gmail.com>
2018-03-01 14:55:56 -0500

    added navbar/basic styling to angular app

--- AppointmentScheduler/client/src/app/app.module.ts
+++ AppointmentScheduler/client/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, NavbarComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
