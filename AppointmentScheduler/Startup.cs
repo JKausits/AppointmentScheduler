fd8a9295e59c4b598487d5c20a955c17a7728f77
Joe <JoeKausits@gmail.com>
2018-03-18 20:39:40 -0400

    modified database connection string to use appsettings values

--- AppointmentScheduler/Startup.cs
+++ AppointmentScheduler/Startup.cs
@@ -19,76 +19,78 @@ using Microsoft.IdentityModel.Tokens;
 
 namespace AppointmentScheduler
 {
-    public class Startup
+  public class Startup
+  {
+    public Startup(IConfiguration configuration)
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddDbContext<AppointmentSchedulerContext>(options =>
-            {
-                options.UseMySQL("server=localhost;database=appointmentScheduler;user=root;password=root");
-            });
-
-
-            //Configure JWT
-            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
-            {
-                options.TokenValidationParameters = new TokenValidationParameters { ValidateIssuer = false, ValidateAudience = false, ValidateLifetime = false, ValidateIssuerSigningKey = true, IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["JWT:SecurityKey"])) };
-            });
-
-            //Enabling CORS
-            var corsBuilder = new CorsPolicyBuilder();
-            corsBuilder.AllowAnyHeader();
-            corsBuilder.AllowAnyMethod();
-            corsBuilder.AllowAnyOrigin();
-            corsBuilder.AllowCredentials();
-
-            services.AddCors(options =>
-            {
-                options.AddPolicy("SiteCorsPolicy", corsBuilder.Build());
-            });
-
-            //Configure to use DI for Email Settings
-            services.Configure<EmailSettings>(Configuration.GetSection("EmailSettings"));
-
-            //Configure to use DI for Email Service
-            services.AddTransient<EmailService>();
-
-            services.AddMvc();
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            app.UseAuthentication();
-            app.UseCors("SiteCorsPolicy");
-            app.Use(async (context, next) =>
-            {
-                await next();
-
-                if (context.Response.StatusCode == 404 &&
-                    !Path.HasExtension(context.Request.Path.Value) &&
-                    !context.Request.Path.Value.StartsWith("/api/"))
-                {
-                    context.Request.Path = "/index.html";
-                    await next();
-                }
-            });
-            app.UseStaticFiles();
-            app.UseMvc();
-        }
-
-       
+      Configuration = configuration;
     }
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+      //Configure Database Connection
+      String connectionString = String.Format("server={0};database={1};user={2};password={3}", Configuration["DatabaseSettings:server"], Configuration["DatabaseSettings:database"], Configuration["DatabaseSettings:user"], Configuration["DatabaseSettings:password"]);
+      services.AddDbContext<AppointmentSchedulerContext>(options =>
+      {
+        options.UseMySQL(connectionString);
+      });
+
+
+      //Configure JWT
+      services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+      {
+        options.TokenValidationParameters = new TokenValidationParameters { ValidateIssuer = false, ValidateAudience = false, ValidateLifetime = false, ValidateIssuerSigningKey = true, IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["JWT:SecurityKey"])) };
+      });
+
+      //Enabling CORS
+      var corsBuilder = new CorsPolicyBuilder();
+      corsBuilder.AllowAnyHeader();
+      corsBuilder.AllowAnyMethod();
+      corsBuilder.AllowAnyOrigin();
+      corsBuilder.AllowCredentials();
+
+      services.AddCors(options =>
+      {
+        options.AddPolicy("SiteCorsPolicy", corsBuilder.Build());
+      });
+
+      //Configure to use DI for Email Settings
+      services.Configure<EmailSettings>(Configuration.GetSection("EmailSettings"));
+
+      //Configure to use DI for Email Service
+      services.AddTransient<EmailService>();
+
+      services.AddMvc();
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+      app.UseAuthentication();
+      app.UseCors("SiteCorsPolicy");
+      app.Use(async (context, next) =>
+      {
+        await next();
+
+        if (context.Response.StatusCode == 404 &&
+                  !Path.HasExtension(context.Request.Path.Value) &&
+                  !context.Request.Path.Value.StartsWith("/api/"))
+        {
+          context.Request.Path = "/index.html";
+          await next();
+        }
+      });
+      app.UseStaticFiles();
+      app.UseMvc();
+    }
+
+
+  }
 }
